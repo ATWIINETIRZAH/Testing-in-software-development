0ec079d606ee1217eb8c3e0dbe18a975905e471a
ATWIINETIRZAH <tirzahatwiine5@gmail.com>
2025-03-17 01:01:10 +0300

    feat: added class user👯

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+# # Create your models here.
+# from django.db import models
+
+# class User(models.Model):
+#     name = models.CharField(max_length=100)
+#     email = models.EmailField()
+#     # other fields...
+
+
+from django.db import models
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
